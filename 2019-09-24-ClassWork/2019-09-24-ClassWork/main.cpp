3a878050ba78b415951cc8959e3a7e4127be4c70
AEscobar516 <55443534+AEscobar516@users.noreply.github.com>
2019-09-24 08:42:17 -0400

    Add files via upload

---
+++ 2019-09-24-ClassWork/2019-09-24-ClassWork/main.cpp
@@ -0,0 +1,30 @@
+// Name:Alexander Escobar
+// Intro to Computer Science
+// Period 1
+// Mr. Williams
+// 2019-09-24-ClassWork
+
+#include <iostream>
+
+int main()
+{
+	int alexTheGenius = 10;
+	int a = 9;
+	char l = 8;
+	bool e = 7;
+	double x = 6;
+	int g = 5;
+	char o = 4;
+	bool d = 3;
+	double h = 2;
+	int y = 1;
+	std::cout << a << std::endl;
+	std::cout << l << std::endl;
+	std::cout << e << std::endl;
+	std::cout << x << std::endl;
+	std::cout << g << std::endl;
+	std::cout << o << std::endl;
+	std::cout << d << std::endl;
+	std::cout << h << std::endl;
+	std::cout << y << std::endl;
+}
