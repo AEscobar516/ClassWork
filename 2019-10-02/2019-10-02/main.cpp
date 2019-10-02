f1c8db6046efd0a3c94fad0d743827a00ea7ab9d
AEscobar516 <55443534+AEscobar516@users.noreply.github.com>
2019-10-02 08:40:21 -0400

    Add files via upload

---
+++ 2019-10-02/2019-10-02/main.cpp
@@ -0,0 +1,23 @@
+// Name: Alexander Escobar
+// Intro To Comp Sci
+// Period 1
+// Mr. Williams
+#include <iostream>
+
+int main()
+{
+	// These are Charcters
+	char letterA = 'A';
+	char letterB = 'h';
+	char lmnop = 'C';
+	//These are Integers
+	short alex = 4;
+	int periods = 10;
+	long long escobar = 8;
+	//These are bools
+	bool iAmTheBest = true;
+	bool amISmart = true;
+	//These are real numbers
+	float e = 4;
+	double o = 81;
+ }
