b229edaa309a5ed35bfef7690e28d032a1ae6e4f
AEscobar516 <55443534+AEscobar516@users.noreply.github.com>
2019-10-03 08:40:11 -0400

    Add files via upload

---
+++ 2019-10-03/2019-10-03/main.cpp
@@ -0,0 +1,16 @@
+// Name: Alexander Escobar
+// Intro to comp sci
+// Period 1
+// Mr. Williams
+// 2019-10-03-ClassWork
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Enter your favorite: ";
+	int favoriteNumber;
+	cin >> favoriteNumber;
+	cout << "Cool, " << favoriteNumber << " is my favorite number too!" << endl;
+}
